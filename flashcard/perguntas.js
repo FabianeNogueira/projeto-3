976941ea9ab60b7e7d5784fd7e0125979a01ff0f
FabianeNogueira <fabiane.nogueira@escola.pr.gov.br>
2025-08-14 11:44:14 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Ciência',
+    'Qual é o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é Júpiter.'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual autor criou o personagem Sherlock Holmes?',
+    'Sherlock Holmes foi criado por Arthur Conan Doyle.'
+)
+
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país da América do Sul?',
+    'O maior país da América do Sul é o Brasil.',
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é o animal terrestre mais rápido do mundo?',
+    'O animal terrestre mais rápido do mundo é o Guepardo.',
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a queda do muro de Berlim?',
+    'A queda aconteceu em 1989.',
+)
+
+criaCartao(
+    'Música',
+    'Em qual ano a banda britânica Panchiko foi formada?',
+    'Entre 1997 e 1998.',
+)
+
+criaCartao(
+    'Química',
+    'O que é uma molécula?',
+    'É a menor partícula de uma substância que mantém as propriedades químicas dessa substância.',
+)
